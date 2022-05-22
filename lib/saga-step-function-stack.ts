86690d2a45a2fbb8017fbd8f4b8dddfdd08f45bd
martinKindall <mart256@gmail.com>
2022-05-22 18:12:28 +0200

    Created table setup and partially flight system functions

--- lib/saga-step-function-stack.ts
+++ lib/saga-step-function-stack.ts
@@ -1,19 +1,43 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class SagaStepFunctionStack extends Stack {
+  private table: dynamodb.Table;
+
+  private reserveFlightLambda: lambda.Function;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'SagaStepFunctionQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    this.dynamoDbSetup();
+
+    this.flightFunctions();
+  }
+
+  private dynamoDbSetup() {
+    this.table = new dynamodb.Table(this, 'Bookings', {
+      partitionKey: {name: 'pk', type: dynamodb.AttributeType.STRING},
+      sortKey: {name: 'sk', type: dynamodb.AttributeType.STRING},
+    });
+  }
+
+  private flightFunctions() {
+    this.reserveFlightLambda = this.createLambda('reserveFlightLambdaHandler', 'flights/reserveFlight.handler');
+  }
+
+  private createLambda(id: string, handler: string) {
+    const newLambda = new lambda.Function(this, id, {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-fns'),
+      handler,
+      environment: {
+        TABLE_NAME: this.table.tableName
+      }
     });
 
-    const topic = new sns.Topic(this, 'SagaStepFunctionTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    this.table.grantReadWriteData(newLambda);
+    return newLambda;
   }
 }
